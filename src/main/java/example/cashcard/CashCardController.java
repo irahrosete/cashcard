4d8709e03a21931effefa0ce7c3dff2dd9ceffdf
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-05-28 18:00:42 +1000

    create cash card controller

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,15 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+    @GetMapping("/{id}")
+    public ResponseEntity<String> findById() {
+        return ResponseEntity.ok("{}");
+    }
+}
